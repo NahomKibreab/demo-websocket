e34f7ccb89d3428a7592be3aad8412ec2d51e6ad
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-04-07 22:15:11 -0700

    Update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
+const sockets = require('./sockets');
 
 const app = express();
 const server = http.Server(app);
-const io = socketio(server);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -13,30 +12,10 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static("public"));
-addSocketListeners(io);
+
+// Handle webSocket connections
+sockets.listen(server);
 
 server.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
-
-// Add a web socket connection listener
-function addSocketListeners(io) {
-  io.on('connection', (socket) => {
-    // This socket param is the sending socke. Has a unique ID
-    // We could save the ID's and associate with a specific client
-    console.log("connected:  ", socket.id);
-
-    socket.on('disconnect', () => {
-      console.log("disconnect: ", socket.id);
-    });
-
-    socket.on('chat message', msg => {
-      // Send any received message to all 
-      io.emit('chat message', "Received: " + msg);
-
-      // Send private message back to the sender
-      io.to(socket.id).emit('chat message', 'Private reply');
-    });
-  });
-
-};

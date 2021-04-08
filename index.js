f8f96e322de358ed03cd4d6e3f6a720144562e6b
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-04-07 22:04:55 -0700

    Update

--- index.js
+++ index.js
@@ -13,24 +13,30 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static("public"));
-
-io.on('connection', (socket) => {
-  // socket param the sending socket with unique ID
-  console.log("connected:  ", socket.id);
-
-  socket.on('disconnect', () => {
-    console.log("disconnect: ", socket.id);
-  });
-
-  socket.on('chat message', msg => {
-    // Send any received message to all 
-    io.emit('chat message', "Received: " + msg);
-
-    // Send private message back to the sender
-    io.to(socket.id).emit('chat message', 'Private reply');
-  });
-});
+addSocketListeners(io);
 
 server.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
+// Add a web socket connection listener
+function addSocketListeners(io) {
+  io.on('connection', (socket) => {
+    // socket param the sending socket with unique ID
+    // We could save the ID's and associate with a specific client
+    console.log("connected:  ", socket.id);
+
+    socket.on('disconnect', () => {
+      console.log("disconnect: ", socket.id);
+    });
+
+    socket.on('chat message', msg => {
+      // Send any received message to all 
+      io.emit('chat message', "Received: " + msg);
+
+      // Send private message back to the sender
+      io.to(socket.id).emit('chat message', 'Private reply');
+    });
+  });
+
+};

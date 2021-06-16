06d1e4a62a95238f7a95c6eef705df309a35aaf5
Gary J <github@accu.ca>
2021-06-15 21:45:10 -0700

    Update

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,46 +1,48 @@
-$(function () {
+(($) => {    // Wraps everything in an IIFE, including $
 
-  const socket = setupSocket();
+  $(function () {
+    const socket = setupSocket();
 
-  $("#send").on('click', function (event) {
-    send(socket, $("input").val());
+    $("#send").on('click', function (event) {
+      send(socket, $("input").val());
+    });
+
+    $("#clear").on('click', function (event) {
+      $("#messages").empty();
+    });
   });
 
-  $("#clear").on('click', function (event) {
-    $("#messages").empty();
-  });
-});
+  // Send message to the server
+  const send = function (socket, text) {
+    if (text) {
+      socket.emit('chat message', text);
+    }
+  };
 
-// Send message to the server
-const send = function (socket, text) {
-  if (text) {
-    socket.emit('chat message', text);
-  }
-};
+  // Create socket and add listeners
+  const setupSocket = function () {
 
-// Create socket and add listeners
-const setupSocket = function () {
+    // "io" comes from the included socket.io file (index.html)
+    const socket = io();
+    socket.on('connect', event => {
+      console.log("connected");
+    });
 
-  // "io" comes from the included socket.io file (index.html)
-  const socket = io();
-  socket.on('connect', event => {
-    console.log("connected");
-  });
+    // Custom socket.io Messages can have different event names
+    socket.on('public message', function (msg) {
+      $("#messages").prepend(`<li>Broadcast: ${msg}</li>`);
+    });
 
-  // Custom socket.io Messages can have different event names
-  socket.on('public message', function (msg) {
-    $("#messages").prepend(`<li>Broadcast: ${msg}</li>`);
-  });
+    // We can handle messages by name 
+    socket.on('private message', function (msg) {
+      $("#messages").prepend(`<li class="private">Received: ${msg}</li>`);
+    });
 
-  // We can handle messages by name 
-  socket.on('private message', function (msg) {
-    $("#messages").prepend(`<li class="private">Received: ${msg}</li>`);
-  });
+    // We can also handle messages sent with no message name
+    socket.on('message', function (msg) {
+      $("#messages").prepend(`<li class="send">Received: ${msg}</li>`);
+    });
 
-  // We can also handle messages sent with no message name
-  socket.on('message', function (msg) {
-    $("#messages").prepend(`<li class="send">Received: ${msg}</li>`);
-  });
-
-  return socket;
-};
+    return socket;
+  };
+})(jQuery);

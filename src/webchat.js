0114fb9dac1cc49b7ec85181f03033d3fd1aaa22
Gary J <github@accu.ca>
2021-08-23 21:20:13 -0700

    update

--- src/webchat.js
+++ src/webchat.js
@@ -14,56 +14,6 @@ const start = function (httpServer) {
 
     socket.on('disconnect', () => {
       console.log("disconnect: ", socket.id);
-
-      // unregister this user
-      for (const user in users) {
-        if (users[user] === socket.id) {
-          console.log("unregistering ", user);
-          delete users[user];
-        }
-      }
-
-      console.log(users);
-    });
-
-    // Handle a "register" message
-    socket.on('register', name => {
-      console.log("register: ", name);
-
-      if (users[name]) {
-        server.to(socket.id).emit('ack', 'You are already registered!');
-        return;
-      }
-
-      // Add user
-      users[name] = socket.id;
-      console.log(users);
-      server.to(socket.id).emit('ack', 'You are now registered!');
-    });
-
-    // Do something whenever a "chat" event is received
-    socket.on('chat', msg => {
-      console.log("message: ", msg);
-
-      // Broadcast received message to all if no "to" received
-      if (!msg.to) {
-        server.emit('public', msg.text);
-        return;
-      }
-
-      const destSocket = users[msg.to];
-      if (!destSocket) {
-        server.to(socket.id).emit('ack', msg.to + ' is not online');
-        return;
-      }
-
-      server.to(destSocket).emit('private', msg.from + ' says: ' + msg.text);
-
-      // Send private message back to the sender (by socket id)
-      server.to(socket.id).emit('ack', 'you sent: ' + msg.text);
-
-      // Alternative: Send generic "message" to this socket only (no  event nanme)
-      // socket.send("send() " + msg.text);
     });
   });
 

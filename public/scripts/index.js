f16815314c7178b319efe2fab919d3a5817385c4
Gary J <github@accu.ca>
2021-08-23 21:31:11 -0700

    Update

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,17 +3,6 @@
   $(function () {
     const socket = setupSocket();;
 
-    $("#send").on('click', function (event) {
-      const to = $("#to").val();
-      const from = $("#name").val();
-      const message = $("#input").val();
-      send(socket, message, from, to);
-    });
-
-    $("#clear").on('click', function (event) {
-      $("#messages").empty();
-    });
-
     $("#register").on('click', function (event) {
       const name = $("#name").val();
       register(socket, name);
@@ -21,13 +10,6 @@
 
   });
 
-  // Send chat message to the server
-  const send = function (socket, text, from, to) {
-    // console.log(from, to, text);
-    if (socket && from && text) {
-      socket.emit('chat', { text, from, to });
-    }
-  };
 
   // Send a register message to the server
   const register = function (socket, name) {
@@ -44,26 +26,6 @@
     const socket = io();
     socket.on('connect', event => {
       console.log("connected");
-    });
-
-    // Custom socket.io Messages can have different event names
-    socket.on('public', function (msg) {
-      $("#messages").prepend(`<li>Broadcast: ${msg}</li>`);
-    });
-
-    // We can handle messages by name 
-    socket.on('private', function (msg) {
-      $("#messages").prepend(`<li class="private">${msg}</li>`);
-    });
-
-    // We can also handle messages sent with no message name
-    socket.on('message', function (msg) {
-      $("#messages").prepend(`<li class="send">${msg}</li>`);
-    });
-
-    // We can also handle messages sent with no message name
-    socket.on('ack', function (msg) {
-      $("#messages").prepend(`<li class="ack">>> ${msg}</li>`);
     });
 
     return socket;

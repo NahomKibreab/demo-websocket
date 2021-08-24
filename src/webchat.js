14a0049e0c4101b3f8ed3b9f2f1e2baecd64c9b2
Gary J <github@accu.ca>
2021-08-23 21:28:53 -0700

    Update

--- src/webchat.js
+++ src/webchat.js
@@ -22,11 +22,10 @@ const start = function (httpServer) {
           delete users[user];
         }
       }
-
       console.log(users);
     });
 
-    // Handle a "register" message
+    // Handle a "register" event
     socket.on('register', name => {
       console.log("register: ", name);
 
@@ -41,32 +40,7 @@ const start = function (httpServer) {
       server.to(socket.id).emit('ack', 'You are now registered!');
     });
 
-    // Do something whenever a "chat" event is received
-    socket.on('chat', msg => {
-      console.log("message: ", msg);
-
-      // Broadcast received message to all if no "to" received
-      if (!msg.to) {
-        server.emit('public', msg.text);
-        return;
-      }
-
-      const destSocket = users[msg.to];
-      if (!destSocket) {
-        server.to(socket.id).emit('ack', msg.to + ' is not online');
-        return;
-      }
-
-      server.to(destSocket).emit('private', msg.from + ' says: ' + msg.text);
-
-      // Send private message back to the sender (by socket id)
-      server.to(socket.id).emit('ack', 'you sent: ' + msg.text);
-
-      // Alternative: Send generic "message" to this socket only (no  event nanme)
-      // socket.send("send() " + msg.text);
-    });
   });
-
 };
 
 module.exports = { start };

b0ebee929c6de31c894f0efdb9f6a95e04849ddc
Gary J <github@accu.ca>
2021-09-20 16:17:21 -0700

    Update

--- src/socket.js
+++ src/socket.js
@@ -2,16 +2,17 @@ const socketio = require('socket.io');
 
 let connected = 0;
 
-const sendStatus = function(server) {
-  const status = { connected };
-  console.log(status);
-  server.emit('status', status);
-};
-
 // Web socket connection listener
 const start = function(httpServer) {
 
   const server = socketio(httpServer);
+
+  // Can use a closure.  server is remembered
+  const sendStatus = function() {
+    const status = { connected };
+    console.log(status);
+    server.emit('status', status);
+  };
 
   server.on('connection', (socket) => {
     // This socket param is the sending socket. Has a unique ID (socket.id)
@@ -20,12 +21,12 @@ const start = function(httpServer) {
 
     server.to(socket.id).emit('notify', `Connected ( ${socket.id} )`);
     connected++;
-    sendStatus(server);
+    sendStatus();
 
     socket.on('disconnect', () => {
       console.log("disconnect: ", socket.id);
       connected--;
-      sendStatus(server);
+      sendStatus();
     });
   });
 

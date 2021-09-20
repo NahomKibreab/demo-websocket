a98e40fa4018b9081a3c09335577ad753619be7d
Gary J <github@accu.ca>
2021-09-20 15:43:58 -0700

    Update

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 const socketio = require('socket.io');
 
 // Web socket connection listener
-const start = function (httpServer) {
+const start = function(httpServer) {
 
   const server = socketio(httpServer);
 
@@ -9,6 +9,8 @@ const start = function (httpServer) {
     // This socket param is the sending socket. Has a unique ID (socket.id)
     // We can use this ID and associate with a specific used
     console.log("connected:  ", socket.id);
+
+    server.to(socket.id).emit('notify', `Connected ( ${socket.id} )`);
 
     socket.on('disconnect', () => {
       console.log("disconnect: ", socket.id);

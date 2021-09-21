24b713069e4e9c06a0301223530a0a8ffd0ea3c6
Gary J <github@accu.ca>
2021-09-20 18:46:02 -0700

    Update

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,17 +1,17 @@
-(($) => {    // Wraps everything in an IIFE, also protects `$` operator 
+// Wraps everything in an IIFE, also protects `$` operator 
+($ => {
 
-  $(function () {
-    const socket = setupSocket();;
+  $(function() {
+    const socket = setupSocket();
 
-    $("#register").on('click', function (event) {
-      const name = $("#name").val();
-      register(socket, name);
+    // Not really needed since browser will disconnect anyway
+    $(window).unload(function() {
+      socket.disconnect();
     });
-
   });
 
   // Create socket and add listeners
-  const setupSocket = function () {
+  const setupSocket = function() {
 
     // "io" comes from the included socket.io file (index.html)
     const socket = io();
@@ -19,15 +19,19 @@
       console.log("connected");
     });
 
+
+    // Messages can have different event names
+    // handle "notify" events (from server to us)
+    socket.on('notify', function(msg) {
+      $(".notify").html(msg);
+    });
+
+    // handle "status" events
+    socket.on('status', function(msg) {
+      console.log(msg);
+      $(".connected").html(msg.connected);
+    });
+
     return socket;
   };
-
-  // Send a register message to the server
-  const register = function (socket, name) {
-    console.log("register");
-    if (socket && name) {
-      socket.emit('register', name);    // Send a 'register' event
-    }
-  };
-
 })(jQuery);
--- src/socket.js
+++ src/socket.js
@@ -1,40 +1,35 @@
 const socketio = require('socket.io');
 
-const users = {};
+let connected = 0;
 
 // Web socket connection listener
-const start = function (httpServer) {
+const start = function(httpServer) {
 
   const server = socketio(httpServer);
 
+  // Can use a closure.  server is remembered
+  const sendStatus = function() {
+    const status = { connected };
+    console.log(status);
+    server.emit('status', status);
+  };
+
   server.on('connection', (socket) => {
     // This socket param is the sending socket. Has a unique ID (socket.id)
-    // We can save the ID's and associate with a specific used
+    // We can use this ID and associate with a specific used
     console.log("connected:  ", socket.id);
+
+    server.to(socket.id).emit('notify', `Connected [ ${socket.id} ]`);
+    connected++;
+    sendStatus();
 
     socket.on('disconnect', () => {
       console.log("disconnect: ", socket.id);
-
-      // unregister this user
-      for (const user in users) {
-        if (users[user] === socket.id) {
-          console.log("unregistering ", user);
-          delete users[user];
-        }
-      }
-      console.log(users);
+      connected--;
+      sendStatus();
     });
-
-    // Handle a "register" event
-    socket.on('register', name => {
-      console.log("register: ", name);
-
-      // Add user
-      users[name] = socket.id;
-      console.log(users);
-    });
-
   });
+
 };
 
 module.exports = { start };

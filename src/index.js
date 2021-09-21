e13929a48e542950d975f2e6d21e4c08f04b7c91
Gary J <github@accu.ca>
2021-09-20 21:37:32 -0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-const http = require('http');
 const express = require('express');
 const socket = require('./socket');
 
 const app = express();
-const httpServer = http.Server(app);
 const port = process.env.PORT || 8002;
 
 app.use(express.static("public"));
 
-// Handle webSocket connections
-socket.start(httpServer);
-
-httpServer.listen(port, () => {
+const httpServer = app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+// Handle webSocket connections
+socket.start(httpServer);
